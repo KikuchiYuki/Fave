32c0cbe869ecebfe7401a2d2079720890f15036b
shiori <s.tamura.sys23@morijyobi.ac.jp>
2024-10-30 11:25:42 +0900

    とりあえずコミット

--- Time of Fave/ShiftFile/shift/shift.js
+++ Time of Fave/ShiftFile/shift/shift.js
@@ -20,7 +20,7 @@ function modalClose() {
 buttonConfirmDelete.addEventListener('click', confirmDelete);
 function confirmDelete() {
   // ページ遷移する
-  window.location.href = 'shift.html';
+  modal.style.display = 'none';
 }
 
 // キャンセルボタンをクリックしたらマイページに遷移する処理
@@ -34,32 +34,3 @@ document.addEventListener('click', function(e) {
     modalClose();
   }
 });
-
-
-document.getElementById("fileInput").addEventListener("change", updateFileName);
-
-function updateFileName() {
-    const fileInput = document.getElementById('fileInput');
-    const fileLabel = document.getElementById('fileLabel');
-
-    if (fileInput.files.length > 0) {
-        // 選択されたファイルの名前をlabelに表示
-        fileLabel.textContent = fileInput.files[0].name;
-    } else {
-        // ファイルが選択されていない場合はデフォルトメッセージを表示
-        fileLabel.textContent = "ファイルを選択してください";
-    }
-}
-
-// 時間データの例 (実際はバックエンドから取得)
-const date1 = new Date('2024-09-18T09:00:00');  // 午前9時
-const date2 = new Date('2024-09-18T15:00:00');  // 午後3時
-
-// 時間だけをゼロなしで取得
-const time1 = date1.getHours() + ':' + String(date1.getMinutes()).padStart(2, '0');
-const time2 = date2.getHours() + ':' + String(date2.getMinutes()).padStart(2, '0');
-
-// HTMLに時間を挿入
-document.querySelector('.time-container .time:nth-child(1)').textContent = time1;
-document.querySelector('.time-container .time:nth-child(2)').textContent = time2;
-
