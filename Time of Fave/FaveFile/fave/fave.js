a1059b6b8f148882e1d45b7ed53eb9a460655c24
shiori <s.tamura.sys23@morijyobi.ac.jp>
2024-11-15 10:30:50 +0900

    fave完成

--- Time of Fave/FaveFile/fave/fave.js
+++ Time of Fave/FaveFile/fave/fave.js
@@ -1,38 +1,21 @@
-// Aボタン
-const buttonA = document.getElementById('a_btn');
+document.addEventListener("DOMContentLoaded", () => {
+    // フォームの表示切替
+    const checkbox = document.getElementById('switch'); 
+    const title = document.querySelector('.title');
+    const galleryForm = document.getElementById('gallery');
+    const listForm = document.getElementById('list');
 
-// Bボタン
-const buttonB = document.getElementById('b_btn');
+    checkbox.addEventListener('click', () => {
+        if (checkbox.checked) {
+            galleryForm.style.display = 'none';
+            listForm.style.display = 'block';
+        } else {
+            galleryForm.style.display = 'block';
+            listForm.style.display = 'none';
+        }
+    });
 
-// A画面
-const screenA = document.querySelector('.a_screen');
+    // 初期状態でイベントフォームを非表示
+    listForm.style.display = 'none';
 
-// B画面
-const screenB = document.querySelector('.b_screen');
-
-
-
-buttonA.addEventListener('click',function(){   // Aボタンをクリックした時
-    buttonA.classList.remove('active');        // 'buttonA' から 'active' が外される
-    buttonB.classList.remove('active');        // 'buttonB' から 'active' が外される 
-});
-
-buttonB.addEventListener('click',function(){   // Bボタンをクリックした時
-    buttonA.classList.add('active');           // 'buttonA' に 'active' が追加される
-    buttonB.classList.add('active');           // 'buttonB' に 'active' が追加される
-});
-
-
-buttonA.addEventListener('click',function(){   // Aボタンをクリックした時
-    buttonA.classList.remove('active');        // 'buttonA' から 'active' が外される
-    buttonB.classList.remove('active');        // 'buttonB' から 'active' が外される
-    screenA.classList.remove('active');        // 'screenA' から 'active' が外される
-    screenB.classList.remove('active');        // 'screenB' から 'active' が外される
-});
-
-buttonB.addEventListener('click',function(){   // Bボタンをクリックした時
-    buttonA.classList.add('active');           // 'buttonA' に 'active' が追加される
-    buttonB.classList.add('active');           // 'buttonB' に 'active' が追加される
-    screenA.classList.add('active');           // 'screenA' に 'active' が追加される
-    screenB.classList.add('active');           // 'screenB' に 'active' が追加される
 });
